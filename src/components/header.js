54112cc5d667f562889a923926a8ff6d0d246288
Victor Chan <victor.chan@publicis.sapient.com>
2019-01-11 20:41:32 -0500

    Added some a11y properties for mobile menu to build onto when building out mobile menu.

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropType from 'prop-types';
 import { graphql, StaticQuery } from 'gatsby';
+import { IconContext } from 'react-icons';
 import { FaBars } from 'react-icons/fa';
 import '../styles/header.scss';
 import logo from '../../static/logo-almex-vert.svg';
@@ -24,9 +25,12 @@ const Header = ({ activeSection }) => (
             <img src={logo} width="50px" alt="Almex Group" />
           </span>
           <div className="active-section-mobile">{nav.labels[activeSection]}</div>
-          <span>
-            <FaBars />
-          </span>
+          <IconContext.Provider value={{ className: 'menu-icon' }}>
+            <button type="button" className="mobile-menu">
+              <FaBars aria-hidden />
+              <span className="sr-only">Open menu</span>
+            </button>
+          </IconContext.Provider>
         </div>
       );
     }}
