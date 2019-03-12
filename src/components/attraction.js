f4b28c6f7016cb4dced6219296b45e1ee34f150d
Victor Chan <victor.chan@almex.com>
2019-03-12 13:28:46 -0400

    Improved initial anim; setup for text staggered animation.

--- src/components/attraction.js
+++ src/components/attraction.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { TimelineLite, CSSPlugin, Circ, Back } from 'gsap';
+import PropTypes from 'prop-types';
+import { TimelineLite, CSSPlugin, Back } from 'gsap';
+import shortid from 'shortid';
 
 // eslint-disable-next-line no-unused-vars
 const myPlugins = [CSSPlugin];
@@ -12,6 +14,9 @@ class Attraction extends React.Component {
     this.outerCircleTween = React.createRef();
     this.circleTextTween = React.createRef();
     this.attractAnimation = new TimelineLite({ paused: true });
+    this.textTweens = [];
+
+    // console.log(props.attractText);
   }
 
   componentDidMount() {
@@ -19,22 +24,44 @@ class Attraction extends React.Component {
       .delay(0.8)
       .fromTo(
         this.innerCircleTween,
-        0.5,
+        0.7,
         { scale: 0.1, autoAlpha: 0 },
         {
           scale: 1,
           autoAlpha: 1,
           transformOrigin: '40 40',
+          // svgOrigin: '40 40',
           ease: Back.easeOut.config(3),
         },
       )
-      // .fromTo(
-      //   this.circleTextTween,
-      //   0.5,
-      //   { scale: 1.5, autoAlpha: 0 },
-      //   { scale: 1, autoAlpha: 1, transformOrigin: '0 0', ease: Circ.easeOut },
-      // )
-      .fromTo(this.gsapELement, 1.6, { x: 1000, y: 10 }, { x: 0, ease: Circ.easeOut })
+      .addLabel('start')
+      .fromTo(
+        this.outerCircleTween,
+        0.5,
+        { scale: 0.1, autoAlpha: 0 },
+        {
+          scale: 1,
+          autoAlpha: 1,
+          transformOrigin: '70 70',
+          // svgOrigin: '40 40',
+          ease: Back.easeOut.config(1),
+        },
+        '-=0.5',
+      )
+      .from(
+        this.circleTextTween,
+        0.5,
+        { scale: 2, autoAlpha: 0, transformOrigin: '70 70', ease: Back.easeOut.config(1) },
+        // { scale: 1, autoAlpha: 1, transformOrigin: '0 0', ease: Circ.easeOut },
+      )
+      // .fromTo(this.gsapELement, 1.6, { x: 1000, y: 10 }, { x: 0, ease: Circ.easeOut }, '+=1')
+      .staggerFromTo(
+        this.textTweens,
+        0.8,
+        { x: 200, autoAlpha: 0 },
+        { x: 0, autoAlpha: 1, ease: Back.easeOut.config(1) },
+        1,
+      )
       // .to(this.gsapELement, 0.5, { y: 100, rotation: 90 })
       .play();
   }
@@ -43,87 +70,111 @@ class Attraction extends React.Component {
     const outerStyle = {
       fill: 'none',
       stroke: '#f2a900',
-      strokeWidth: '1px',
+      strokeWidth: '2px',
       strokeMiterlimit: '1.5',
       strokeDasharray: '1, 2',
+      transformOrigin: 'center',
     };
 
     const thisText1 = `What you need`;
     const thisText2 = `to know`;
 
+    const { attractText } = this.props;
     return (
       <>
-        <div
-          className="attraction"
-          ref={elem => {
-            this.gsapELement = elem;
-          }}
-        >
-          This is a line of text
-        </div>
-        <svg
-          width="100%"
-          height="100%"
-          viewBox="0 0 144 144"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          xmlSpace="preserve"
-        >
-          {/* <g id="outer" transform="matrix(1.45537,0,0,1.45537,-26.9133,-23.8768)">
+        <div className="svg-container">
+          <svg
+            width="144px"
+            height="144px"
+            viewBox="0 0 144 144"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            xmlSpace="preserve"
+          >
+            {/* <g id="outer" transform="matrix(1.45537,0,0,1.45537,-26.9133,-23.8768)">
             <circle cx="67.964" cy="65.878" r="48.098" style={outerStyle} />
           </g> */}
-          <g id="outer">
-            <circle cx="72" cy="72" r="72" style={outerStyle} />
-          </g>
-          <defs>
-            <path
-              id="textPath1"
-              d="M2,72C2,38.353 29.554,1.798 72,2C118.787,2.223 142.308,42.272 142,72C141.668,103.976 117.973,142 72,142C28.135,142 2,105.647 2,72Z"
-              style={{ fill: 'none', stroke: 'red' }}
+            <circle
+              cx="72"
+              cy="72"
+              r="70"
+              style={outerStyle}
+              ref={elem => {
+                this.outerCircleTween = elem;
+              }}
             />
-            <path
-              id="textPath2"
-              d="M306 621.25c131.971 0 224.006-105.615 225.25-225.25 1.251-119.635-94.938-224.627-225.25-225.25C183.308 170.166 80.75 272.729 80.75 396S185.337 621.25 306 621.25z"
-              style={{ fill: 'none', stroke: 'blue' }}
-              transform="matrix(0 .23529 -.23529 0 165.174 0)"
+            <defs>
+              <path
+                id="textPath1"
+                d="M2,72C2,38.353 29.554,1.798 72,2C118.787,2.223 142.308,42.272 142,72C141.668,103.976 117.973,142 72,142C28.135,142 2,105.647 2,72Z"
+                style={{ fill: 'none', stroke: 'red' }}
+              />
+              <path
+                id="textPath2"
+                d="M306 621.25c131.971 0 224.006-105.615 225.25-225.25 1.251-119.635-94.938-224.627-225.25-225.25C183.308 170.166 80.75 272.729 80.75 396S185.337 621.25 306 621.25z"
+                style={{ fill: 'none', stroke: 'blue' }}
+                transform="matrix(0 .23529 -.23529 0 165.174 0)"
+              />
+            </defs>
+            <g
+              style={{ transformOrigin: 'center' }}
+              ref={elem => {
+                this.circleTextTween = elem;
+              }}
+            >
+              <text id="attractText1">
+                <textPath xlinkHref="#textPath1">
+                  <tspan dy="1.4rem" dx="20">
+                    {thisText1}
+                  </tspan>
+                </textPath>
+              </text>
+              <text id="attractText2">
+                <textPath xlinkHref="#textPath2">
+                  <tspan dy="0.5rem" dx="45">
+                    {thisText2}
+                  </tspan>
+                </textPath>
+              </text>
+            </g>
+            <circle
+              id="core"
+              cx="72"
+              cy="72"
+              r="40"
+              style={{ fill: 'rgb(242, 169, 0)', transformOrigin: 'center' }}
+              ref={elem => {
+                this.innerCircleTween = elem;
+              }}
             />
-          </defs>
-          <g
-            ref={elem => {
-              this.circleTextTween = elem;
-            }}
-            style={{ transformOrigin: 'center' }}
-          >
-            <text id="attractText1">
-              <textPath xlinkHref="#textPath1">
-                <tspan dy="1.4rem" dx="20">
-                  {thisText1}
-                </tspan>
-              </textPath>
-            </text>
-            <text id="attractText2">
-              <textPath xlinkHref="#textPath2">
-                <tspan dy="0.5rem" dx="45">
-                  {thisText2}
-                </tspan>
-              </textPath>
-            </text>
-          </g>
-          <circle
-            id="core"
-            cx="72"
-            cy="72"
-            r="40"
-            style={{ fill: 'rgb(242, 169, 0)', transformOrigin: 'center' }}
-            ref={elem => {
-              this.innerCircleTween = elem;
-            }}
-          />
-        </svg>
+          </svg>
+        </div>
+        <div className="text-container">
+          <ul>
+            {attractText.map((elem, idx) => (
+              <li
+                key={shortid.generate()}
+                ref={li => {
+                  this.textTweens[idx] = li;
+                }}
+              >
+                {elem}
+              </li>
+            ))}
+          </ul>
+        </div>
       </>
     );
   }
 }
+
+Attraction.defaultProps = {
+  attractText: [],
+};
+
+Attraction.propTypes = {
+  attractText: PropTypes.arrayOf(PropTypes.string),
+};
 
 export default Attraction;
