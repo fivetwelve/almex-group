f335302812a3a197a87cd6ae8c020586f677f3a6
Victor Chan <victor.chan@almex.com>
2019-03-18 16:01:26 -0400

    Added @babel/polyfill for IE11 fix attempt.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
+// require('gatsby-plugin-polyfill-io');
+require('@babel/polyfill');
 require('./src/styles/_variables.scss');
 require('./src/styles/global.scss');
 require('./src/utils/gsap/DrawSVGPlugin');
-// require('css-vars-ponyfill');
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const proxy = require('http-proxy-middleware');
-// const cssVariables = require('postcss-css-variables');
 const cssVariables = require('postcss-custom-properties');
 
 module.exports = {
@@ -26,22 +25,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-sass`,
       options: {
         postCssPlugins: [cssVariables()],
       },
     },
-    // 'gatsby-plugin-postcss',
-    // {
-    //   resolve: `gatsby-plugin-postcss-sass`,
-    //   options: {
-    //     postCssPlugins: [cssVariables()],
-    //     // precision: 8, // SASS default: 5
-    //   },
-    // },
-    // `css-vars-ponyfill`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
