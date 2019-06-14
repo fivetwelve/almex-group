c3fad878d4503b8e60a5d90cc6773d7524796a05
Victor Chan <victor.chan@almex.com>
2019-06-14 12:17:04 -0400

    Triggering a new dev build.

--- src/templates/contact-template.js
+++ src/templates/contact-template.js
@@ -44,7 +44,6 @@ const ContactTemplate = ({ data, pageContext }) => {
       region={region}
       title=""
     >
-      {/* <ContactMap /> */}
       <Location>
         {({ location }) => (
           <>
