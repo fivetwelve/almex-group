36977bfea4bc447ff792b153d4a1748e8e0dbfaa
Victor Chan <victor.chan@almex.com>
2019-08-15 15:39:26 -0400

    Minor CSS updates to nav and homepage.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography';
 
 const typography = new Typography({
-  baseFontSize: '14px',
+  baseFontSize: '16px',
   baseLineHeight: 1,
   // headerFontFamily: ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
   // bodyFontFamily: ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
