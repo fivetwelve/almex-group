58435c50d39693e39d0d592d0183ad4d00173f78
Victor Chan <victor.chan@almex.com>
2019-08-15 14:50:42 -0400

    Initial switch to Nunito Sans, Tahoma typefaces.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,11 +5,11 @@ const typography = new Typography({
   baseLineHeight: 1,
   // headerFontFamily: ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
   // bodyFontFamily: ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
-  headerFontFamily: ['Arial', 'sans-serif'],
-  bodyFontFamily: ['Arial', 'sans-serif'],
+  headerFontFamily: ['Nunito Sans', 'Tahoma', 'sans-serif'],
+  bodyFontFamily: ['Nunito Sans', 'Tahoma', 'sans-serif'],
   overrideStyles: () => ({
     h1: {
-      fontWeight: 100,
+      fontWeight: 700,
     },
   }),
 });
