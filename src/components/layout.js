b6b943aaa9219d2e8c286963ff8601c29c9daab9
Victor Chan <victor.chan@almex.com>
2020-01-20 15:12:46 -0500

    Switched react-helmet to react-helmet-async due to React 16+ compatibility issues.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import { Waypoint } from 'react-waypoint';
 import { Location } from '@reach/router';
 import { IconContext } from 'react-icons';
