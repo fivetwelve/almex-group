d2f965b8b437cd0b4a1c721b1d23610ac5b18030
Victor Chan <victor.chan@almex.com>
2020-03-20 15:16:15 -0400

    IE11 fix for brands overlapping content on About page.

--- src/components/brandBanner.js
+++ src/components/brandBanner.js
@@ -5,62 +5,64 @@ import { createLink } from '../utils/functions';
 import { BRANDS, PAGE_TYPES } from '../constants';
 
 const BrandBanner = ({ brands, location }) => (
-  <div className="brands">
-    {brands.map(brand => {
-      let brandType = '';
-      let productBrand = '';
-      switch (brand.pageType) {
-        case PAGE_TYPES.INSTITUTE:
-          brandType = brand.institute;
-          break;
-        case PAGE_TYPES.LANDING:
-          brandType = brand.landing;
-          break;
-        case PAGE_TYPES.SERVICES:
-          brandType = brand.services;
-          break;
-        default:
-          break;
-      }
-      switch (brandType.brand) {
-        case BRANDS.ALMEX_IN_A_BOX:
-          productBrand = 'almex-box';
-          break;
-        case BRANDS.BAT:
-          productBrand = 'bat';
-          break;
-        case BRANDS.LIGHTWEIGHT:
-          productBrand = 'lightweight';
-          break;
-        case BRANDS.CMI:
-          productBrand = 'cmi';
-          break;
-        case BRANDS.EMSYS:
-          productBrand = 'emsys';
-          break;
-        case BRANDS.FUSION:
-          productBrand = 'fusion';
-          break;
-        case BRANDS.ALMEX_INSTITUTE:
-          productBrand = 'institute';
-          break;
-        case BRANDS.GLOBAL_SERVICES:
-          productBrand = 'knight';
-          break;
-        case BRANDS.VOTECH:
-          productBrand = 'votech';
-          break;
-        default:
-          break;
-      }
-      return (
-        <div className={`brand ${productBrand}`} key={brand.slug}>
-          <Link to={createLink(location, brand.slug)}>
-            <span className="sr-only">{brandType.title}</span>
-          </Link>
-        </div>
-      );
-    })}
+  <div className="brands-container">
+    <div className="brands">
+      {brands.map(brand => {
+        let brandType = '';
+        let productBrand = '';
+        switch (brand.pageType) {
+          case PAGE_TYPES.INSTITUTE:
+            brandType = brand.institute;
+            break;
+          case PAGE_TYPES.LANDING:
+            brandType = brand.landing;
+            break;
+          case PAGE_TYPES.SERVICES:
+            brandType = brand.services;
+            break;
+          default:
+            break;
+        }
+        switch (brandType.brand) {
+          case BRANDS.ALMEX_IN_A_BOX:
+            productBrand = 'almex-box';
+            break;
+          case BRANDS.BAT:
+            productBrand = 'bat';
+            break;
+          case BRANDS.LIGHTWEIGHT:
+            productBrand = 'lightweight';
+            break;
+          case BRANDS.CMI:
+            productBrand = 'cmi';
+            break;
+          case BRANDS.EMSYS:
+            productBrand = 'emsys';
+            break;
+          case BRANDS.FUSION:
+            productBrand = 'fusion';
+            break;
+          case BRANDS.ALMEX_INSTITUTE:
+            productBrand = 'institute';
+            break;
+          case BRANDS.GLOBAL_SERVICES:
+            productBrand = 'knight';
+            break;
+          case BRANDS.VOTECH:
+            productBrand = 'votech';
+            break;
+          default:
+            break;
+        }
+        return (
+          <div className={`brand ${productBrand}`} key={brand.slug}>
+            <Link to={createLink(location, brand.slug)}>
+              <span className="sr-only">{brandType.title}</span>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
   </div>
 );
 
