465602bf63653aaba64a0f2e7bca16252b77ec94
Victor Chan <victor.chan@almex.com>
2021-05-31 00:55:04 -0400

    Added loggly to debug getRegion.

--- src/lambda/getRegion.js
+++ src/lambda/getRegion.js
@@ -1,8 +1,18 @@
 // import fetch from 'node-fetch';
 import fetch from 'isomorphic-fetch';
+import loggly from 'node-loggly-bulk';
 import 'dotenv/config';
 
 const ipapiKey = process.env.IPAPI_APIKEY_PRIVATE;
+const logClient = loggly.createClient({
+  token: 'b81b81cb-9289-45c9-bd10-51b22f7b0912',
+  subdomain: 'almex',
+  auth: {
+    username: 'victor-almex@fivetwelve.ca',
+    password: 'stack-trace-1',
+  },
+  json: true,
+});
 
 function checkStatus(response) {
   if (response.ok) {
@@ -13,8 +23,13 @@ function checkStatus(response) {
   }
 }
 
-exports.handler = (event, context, callback) => {
-  const params = JSON.parse(event.body);
+exports.handler = async (event, context, callback) => {
+  logClient.log(event);
+  // console.log('event');
+  // console.log(event);
+  // console.log('context');
+  // console.log(context);
+  // const params = JSON.parse(event.body);
   const response = fetch(`https://ipapi.co/${event.headers['client-ip']}/json/?key=${ipapiKey}`, {
     headers: {
       Accept: 'application/json',
